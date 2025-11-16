ddb18847bbf8a4cffc446e62167c7af713ba9655
João Vieira <mutantinotrue@outlook.com>
2025-11-16 16:39:59 +0000

    Mudando a frase, e ajustando TAG

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 function Home() {
-    return (<h1>Para fazer um torta do zero, primeiro você deve criar o universo</h1>);
-}
-
-function Video() {
-    return (<iframe width="996" height="742" src="https://www.youtube.com/embed/WnOWVSYNMFw" title="Sementes Anhanguera - Comercial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>);
+    return (<div>
+    <h1>Go drinking (vai tomando)</h1>)
+    <iframe width="996" height="742" src="https://www.youtube.com/embed/WnOWVSYNMFw" title="Sementes Anhanguera - Comercial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>)
 }
 
 export default Home;
