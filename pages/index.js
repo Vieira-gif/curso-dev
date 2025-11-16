16d925e27553c734d2b2d68eee8959823a06fe03
João Vieira <mutantinotrue@outlook.com>
2025-11-16 16:33:07 +0000

    Adicionando video

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return (<h1>Para fazer um torta do zero, primeiro você deve criar o universo</h1>)
+    return (<h1>Para fazer um torta do zero, primeiro você deve criar o universo</h1>);
+}
+
+function Video() {
+    return (<iframe width="996" height="742" src="https://www.youtube.com/embed/WnOWVSYNMFw" title="Sementes Anhanguera - Comercial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>);
 }
 
 export default Home;
