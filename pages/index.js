e95a2f9a2b1006e8bf430bed49c8f079ad2fdd7b
João Vieira <mutantinotrue@outlook.com>
2025-11-19 15:42:04 +0000

    utilizando prettier

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 function Home() {
-  return (<div>
-  <h1>Go drinking (vai tomando)</h1>)
-  <iframe width="996" height="742" src="https://www.youtube.com/embed/WnOWVSYNMFw" title="Sementes Anhanguera - Comercial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>)
+  return (
+    <div>
+      <h1>Go drinking (vai tomando)</h1>)
+      <iframe
+        width="996"
+        height="742"
+        src="https://www.youtube.com/embed/WnOWVSYNMFw"
+        title="Sementes Anhanguera - Comercial"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
 
 export default Home;
