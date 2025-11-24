226d2ed0a2c06b8bb82bf052cce7b5478e155fe6
João Vieira <mutantinotrue@outlook.com>
2025-11-24 15:08:29 +0000

    Alterado frase

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import image from "../svg/builder.png";
 function Home() {
   return (
     <div>
-      <h1>Aqui começa um a tela de Construção</h1>
+      <h1>Aqui começa a tela de Construção</h1>
       <p>
         Onde define um nova etapa do profissionalismo da jornada na area de
         programação
       </p>
-      <img src={image} width="500" height="600"></img>
+      <img src='../svg/builder.png' width="500" height="600"></img>
     </div>
   );
 }
