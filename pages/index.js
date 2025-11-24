f9586e1bc72d24df18563e53f4bd06a59869c07d
João Vieira <mutantinotrue@outlook.com>
2025-11-24 14:58:56 +0000

    Alterado frase

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
+import image from "./svg/builder.png";
+
 function Home() {
   return (
     <div>
-      <h1>Go drinking (vai tomando)</h1>)
-      <iframe
-        width="996"
-        height="742"
-        src="https://www.youtube.com/embed/WnOWVSYNMFw"
-        title="Sementes Anhanguera - Comercial"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+      <h1>Aqui começa um a tela de Construção</h1>
+      <p>
+        Onde define um nova etapa do profissionalismo da jornada na area de
+        programação
+      </p>
+      <img src={image} width="500" height="600"></img>
     </div>
   );
 }
