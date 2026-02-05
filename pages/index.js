c89e6b84ec33965ac1877b2e0cc0f2b5cee8582d
João Vieira <mutantinotrue@outlook.com>
2026-02-05 21:56:23 +0000

    alterando o texto do html

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,16 @@ function Home() {
   return (
     <div>
       <h1>Aqui começa a tela de Construção</h1>
-      <p>
-        Onde define um nova etapa do profissionalismo da jornada na area de
-        programação
-      </p>
+      <section>
+        <h2>Endpoint /status</h2>
+        <p>
+          <a>https://www.stacklearn.com.br/api/v1/status</a>
+        </p>
+        <h2>Endpoint /migrations</h2>
+        <p>
+          <a>https://www.stacklearn.com.br/api/v1/migrations</a>
+        </p>
+      </section>
       <img src={image} width="500" height="600"></img>
     </div>
   );
